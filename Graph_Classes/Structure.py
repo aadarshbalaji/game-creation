26c46be1dbfea151978394aa646caa4e452caf9b
aadarshbalaji <150199676+aadarshbalaji@users.noreply.github.com>
2025-03-11 16:47:45 -0700

    created initial graph and node classes

---
+++ Graph_Classes/Structure.py
@@ -0,0 +1,62 @@
+import hashlib
+import json
+class Node:
+    def __init__(self, story, is_end=False):
+        self.story = story
+        self.is_end = is_end
+        self.connections = [None] * 4
+        self.previous = None
+        self.id = self.generate_id(story)
+
+    def generate_id(self, story):
+        return hashlib.sha256(story.encode()).hexdigest()
+    
+    def __repr__(self):
+        return str(self.id)
+
+
+class Graph:
+    def __init__(self):
+        self.adjacency_list = {}
+    
+    def add_node(self, node):
+        if node not in self.adjacency_list:
+            self.adjacency_list[node] = {'parents': set(), 'children': set()}
+    
+    def add_edge(self, parent, child):
+        if parent not in self.adjacency_list:
+            self.add_node(parent)
+        if child not in self.adjacency_list:
+            self.add_node(child)
+        
+        self.adjacency_list[parent]['children'].add(child)
+        self.adjacency_list[child]['parents'].add(parent)
+    
+    def get_parents(self, node):
+        if node in self.adjacency_list:
+            return self.adjacency_list[node]['parents']
+        return None
+    
+    def get_children(self, node):
+        if node in self.adjacency_list:
+            return self.adjacency_list[node]['children']
+        return None
+    
+    def __repr__(self):
+        return json.dumps({str(node): {'parents': list(map(str, data['parents'])), 'children': list(map(str, data['children']))} for node, data in self.adjacency_list.items()}, indent=4)
+
+
+#Example Usage:
+node1 = Node('Once upon a time...')
+node2 = Node('There was a princess...')
+node3 = Node('She lived in a castle...', is_end=True)
+
+graph = Graph()
+graph.add_node(node1)
+graph.add_node(node2)
+graph.add_node(node3)
+
+graph.add_edge(node1, node2)
+graph.add_edge(node1, node3)
+
+print(graph)
